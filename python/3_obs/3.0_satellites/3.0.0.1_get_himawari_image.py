eef5123b7fde229012e1103d079fa6df70e6e5d9
Qinggang Gao <gaoqg229@gmail.com>
2025-03-08 22:34:26 +1100

    8 Mar 2025

--- python/3_obs/3.0_satellites/3.0.0.1_get_himawari_image.py
+++ python/3_obs/3.0_satellites/3.0.0.1_get_himawari_image.py
@@ -1,6 +1,6 @@
 
 
-# qsub -I -q normal -l walltime=10:00:00,ncpus=1,mem=192GB,storage=gdata/v46+gdata/rt52+gdata/ob53+gdata/zv2+gdata/ra22
+# qsub -I -q copyq -l walltime=2:00:00,ncpus=1,mem=20GB,storage=gdata/v46+gdata/rt52+gdata/ob53+gdata/zv2+gdata/ra22
 
 
 # region import packages
@@ -13,140 +13,106 @@ dask.config.set({"array.slicing.split_large_chunks": True})
 from dask.diagnostics import ProgressBar
 pbar = ProgressBar()
 pbar.register()
-from scipy import stats
-import pandas as pd
-from metpy.interpolate import cross_section
-from statsmodels.stats import multitest
-from metpy.calc import pressure_to_height_std, geopotential_to_height
-from metpy.units import units
-import metpy.calc as mpcalc
-import pickle
-from xmip.preprocessing import rename_cmip6, broadcast_lonlat, correct_lon, promote_empty_dims, replace_x_y_nominal_lat_lon, correct_units, correct_coordinates, parse_lon_lat_bounds, maybe_convert_bounds_to_vertex, maybe_convert_vertex_to_bounds, combined_preprocessing
+from skimage.measure import block_reduce
 
 # plot
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from matplotlib.colors import BoundaryNorm
 import cartopy.crs as ccrs
-from matplotlib import cm
 plt.rcParams['pcolor.shading'] = 'auto'
 mpl.rcParams['figure.dpi'] = 600
 mpl.rcParams['axes.linewidth'] = 0.2
 plt.rcParams.update({"mathtext.fontset": "stix"})
-import matplotlib.animation as animation
-import seaborn as sns
-from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
-from matplotlib.ticker import AutoMinorLocator
-import geopandas as gpd
-import matplotlib.patches as mpatches
-from matplotlib.lines import Line2D
+mpl.rc('font', family='Times New Roman', size=10)
 import cartopy.feature as cfeature
+import matplotlib.ticker as mticker
 
 # management
 import os
 import sys  # print(sys.path)
 sys.path.append(os.getcwd() + '/code/gbr_future/module')
-import psutil
-process = psutil.Process()
-# print(process.memory_info().rss / 2**30)
-import string
-import warnings
-warnings.filterwarnings('ignore')
-import re
 import glob
 import time
+from pathlib import Path
 
 # self defined
 from mapplot import (
-    globe_plot,
     regional_plot,
-    ticks_labels,
-    scale_bar,
-    plot_maxmin_points,
-    remove_trailing_zero,
-    remove_trailing_zero_pos,
     )
-
-from namelist import (
-    month,
-    monthini,
-    seasons,
-    seconds_per_d,
-    zerok,
-    panel_labels,
-    era5_varlabels,
-    cmip6_era5_var,
-    )
-
-from component_plot import (
-    rainbow_text,
-    change_snsbar_width,
-    cplot_wind_vectors,
-    cplot_lon180,
-    cplot_lon180_ctr,
-    plt_mesh_pars,
-)
-
-from calculations import (
-    mon_sea_ann,
-    regrid,
-    cdo_regrid,
-    time_weighted_mean)
-
-from statistics0 import (
-    ttest_fdr_control,)
 
 # endregion
 
 
 # region plot himawari data
 
-year=2020
-month=7
-day=1
-hour=12
-minute=0
+start_time = time.perf_counter()
+
+year, month, day, hour, minute = 2025, 3, 7, 12, 0
 ioption='true_color'
 # ioption='night_microphysics'
 
-dfolder = '/g/data/ra22/satellite-products/arc/obs/himawari-ahi/fldk/latest'
-opng = f'figures/3_satellites/3.0_hamawari/3.0.0_image/3.0.0.0 himawari {ioption} {year}{month:02d}{day:02d}{hour:02d}{minute:02d}.png'
+# dfolder = Path('/g/data/ra22/satellite-products/arc/obs/himawari-ahi/fldk/latest')
+dfolder = Path('/g/data/ra22/satellite-products/nrt/obs/himawari-ahi/fldk/latest')
+opng = Path(f'figures/3_satellites/3.0_hamawari/3.0.0_image/3.0.0.0 himawari {ioption} {year}{month:02d}{day:02d}{hour:02d}{minute:02d}.png')
 
-if ioption == 'true_color':
-    bands = ['B01', 'B02', 'B03']
-    plt_text = f'{year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d} UTC\n True Color RGB Himawari 8/9'
-elif ioption == 'night_microphysics':
-    bands = ['B07', 'B13', 'B15']
-    plt_text = f'{year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d} UTC\n Night Microphysics RGB Himawari 8/9'
+band_map = {
+    'true_color': ['B03', 'B02', 'B01'],
+    'night_microphysics': ['B07', 'B13', 'B15']
+}
+bands = band_map[ioption]
+plt_text = f'{year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d} UTC\n{ioption.replace('_', ' ').title()} RGB Himawari 8/9'
 
 channels = {}
 for iband in bands:
     # iband='B03'
     print(f'#-------------------------------- {iband}')
     
-    ifile = sorted(glob.glob(f'{dfolder}/{year}/{month:02d}/{day:02d}/{hour:02d}{minute:02d}/*OBS_{iband}*'))[-1]
-    channels[iband] = xr.open_dataset(ifile)
+    ifile = sorted(Path(dfolder/f'{year}/{month:02d}/{day:02d}/{hour:02d}{minute:02d}').glob(f'*OBS_{iband}*'))
+    if not ifile:
+        print('Warning: No file found')
+        continue
     
-    var_name = [var for var in channels[iband].data_vars if var.startswith('channel_00')][0]
-    channels[iband] = channels[iband][var_name].squeeze()
+    channels[iband] = xr.open_dataset(ifile[-1], chunks={})
+    var_name = next(var for var in channels[iband].data_vars if var.startswith('channel_00'))
+    channels[iband] = channels[iband][var_name].squeeze().values
     
     if iband == 'B03':
-        channels[iband] = channels[iband].coarsen(y=2, x=2, boundary='trim').mean()
+        channels[iband] = block_reduce(channels[iband], block_size=(2, 2), func=np.mean)
 
-extent=[channels[iband].x.min().values, channels[iband].x.max().values, channels[iband].y.min().values, channels[iband].y.max().values]
-transform=ccrs.Geostationary(central_longitude=140.7, satellite_height=35785863.0)
+extent = [-5499500., 5499500., -5499500., 5499500.]
+transform = ccrs.Geostationary(central_longitude=140.7, satellite_height=35785863.0)
 
-if ioption == 'true_color':
-    rgb = np.dstack([channels['B03'], channels['B02'], channels['B01']])
+rgb = np.zeros(channels[bands[0]].shape + (3,), dtype=np.float32)
+for idx, iband in enumerate(bands):
+    print(f'{idx} {iband}')
+    rgb[:, :, idx] = channels[iband]
 
-rgb = np.clip(rgb, 0, 1)
-rgb = np.power(rgb, 1/2.2)
+rgb = rgb.clip(0, 1) ** (1 / 2.2)
+
+end_time = time.perf_counter()
+print(f"Execution time: {end_time - start_time:.6f} seconds")
+
 
 start_time = time.perf_counter()
 
-fig, ax = regional_plot(extent=[80, 200, -60, 60], central_longitude=180, figsize = np.array([6.6, 6.6+0.9])/2.54, border_color='yellow', lw=0.1)
+fig, ax = plt.subplots(figsize=np.array([6.6, 6.6+0.9])/2.54, subplot_kw={'projection': transform})
 
-ax.imshow(rgb, extent=extent, transform=transform, interpolation='none')
+ax.imshow(rgb, extent=extent,
+          transform=transform, rasterized=True,
+          interpolation='none', origin='upper', resample=False)
+
+coastline = cfeature.NaturalEarthFeature(
+    'physical', 'coastline', '10m', edgecolor='yellow',
+    facecolor='none', lw=0.1)
+ax.add_feature(coastline, zorder=2, rasterized=True, alpha=0.75)
+borders = cfeature.NaturalEarthFeature(
+    'cultural', 'admin_0_boundary_lines_land', '10m',
+    edgecolor='yellow', facecolor='none', lw=0.1)
+ax.add_feature(borders, zorder=2, rasterized=True, alpha=0.75)
+ax.gridlines(
+    crs=ccrs.PlateCarree(), lw=0.1, zorder=2, alpha=0.35,
+    color='yellow', linestyle='--',
+    xlocs = np.arange(0, 360 + 1e-4, 10), ylocs=np.arange(-90, 90 + 1e-4, 10))
 
 plt.text(
         0.5, -0.03, plt_text, transform=ax.transAxes, fontsize=8,
@@ -154,17 +120,45 @@ plt.text(
 
 fig.subplots_adjust(left=0.01, right=0.99, bottom=0.9/(6.6+0.9), top=0.99)
 fig.savefig(opng)
+plt.close()
 
 end_time = time.perf_counter()
 print(f"Execution time: {end_time - start_time:.6f} seconds")
 
 
 
-
-import rioxarray as rxr
-orig = rxr.open_rasterio(ifile, masked=True)
-
-
 '''
+#-------------------------------- projection using pyproj
+import pyproj
+# import rioxarray as rxr
+ds = xr.open_dataset(ifile)
+
+iproj = pyproj.CRS.from_proj4(ds.geostationary.proj4)
+oproj = pyproj.CRS.from_epsg(4326)
+transformer = pyproj.Transformer.from_crs(iproj, oproj, always_xy=True)
+
+xx, yy = np.meshgrid(ds.x.values, ds.y.values)
+lon, lat = transformer.transform(xx, yy)
+
+np.nanmax(lon[np.isfinite(lon)])
+np.nanmax(lat[np.isfinite(lat)])
+
+
+dis = 3840000
+print(transformer.transform(3840000, 3840000))
+
+
+# fig, ax = regional_plot(extent=[40, 240, -85, 85], central_longitude=180, figsize = np.array([6.6, 6.6+0.9])/2.54, border_color='yellow', lw=0.1)
+
+ifile = sorted(glob.glob(f'{dfolder}/{year}/{month:02d}/{day:02d}/{hour:02d}{minute:02d}/*OBS_{iband}*'))[-1]
+channels[iband] = channels[iband].coarsen(y=2, x=2, boundary='trim').mean()
+
+rgb = np.clip(rgb, 0, 1)
+rgb **= (1 / 2.2)
+
+rgb = np.dstack([channels['B03'], channels['B02'], channels['B01']])
+
+
+# change projection
 '''
 # endregion
--- shell/0_conda.sh
+++ shell/0_conda.sh
@@ -1,7 +1,7 @@
 
-# source ~/miniconda3/bin/activate
-# conda create --name rcm_gbr
-# source ~/miniconda3/bin/activate rcm_gbr
+# source miniconda3/bin/activate
+# conda create --name rcm_gbr -y
+# source miniconda3/bin/activate rcm_gbr
 # conda config --add channels conda-forge
 
 #-------------------------------- Primary packages
@@ -15,60 +15,49 @@ mamba install cartopy -y
 mamba install cdo -y
 mamba install seaborn -y
 mamba install cdsapi -y
-mamba install haversine -y
 mamba install windrose -y
 mamba install ffmpeg -y
-mamba install mscorefonts -y
-mamba install line_profiler -y
-mamba install rasterio -y
-mamba install rioxarray -y
 mamba install geopandas -y
-mamba install pymannkendall -y
 mamba install fortls -y
-mamba install nose -y
-mamba install cmip6_preprocessing -y
-mamba install xskillscore -y
-mamba install eofs -y
-mamba install lazy_loader -y
-mamba install siphon -y
 mamba install mpl-scatter-density -y
 mamba install intake -y
-mamba install intake-xarray -y
-mamba install dask-gateway -y
-mamba install xarrayutils -y
-mamba install xgcm -y
-mamba install gcsfs -y
-mamba install python-cdo -y
-mamba install numexpr -y
-mamba install pyhdf -y
-mamba install cfgrib -y
-mamba install geopy -y
 mamba install pycircstat -y
-mamba install openpyxl -y
-mamba install Pympler -y
 mamba install pingouin -y
-mamba install intake-esm -y
 mamba install ipython -y
-mamba install pytest -y
-mamba install pint-xarray -y
-mamba install ipykernel -y
-mamba install ipywidgets -y
-mamba install wget -y
 mamba install ncview -y
 mamba install xmip -y
-mamba install satpy -y
-mamba install libgdal-hdf5
-
-pip install SkillMetrics
-pip install igra
-pip install pint-xarray
-pip install findent
-pip install f90nml
+mamba install siphon -y
+mamba install scikit-image -y
 
 #-------------------------------- Secondary packages
-
-# pip install pycircstat failed
-
+# mamba install mscorefonts -y
+# mamba install xskillscore -y
+# mamba install rasterio -y
+# mamba install rioxarray -y
+# mamba install line_profiler -y
+# mamba install haversine -y
+# mamba install nose -y
+# mamba install openpyxl -y
+# mamba install Pympler -y
+# mamba install intake-esm -y
+# mamba install intake-xarray -y
+# mamba install dask-gateway -y
+# mamba install xarrayutils -y
+# mamba install xgcm -y
+# mamba install gcsfs -y
+# mamba install python-cdo -y
+# mamba install numexpr -y
+# mamba install pyhdf -y
+# mamba install cfgrib -y
+# mamba install geopy -y
+# mamba install pytest -y
+# mamba install pint-xarray -y
+# mamba install ipykernel -y
+# mamba install ipywidgets -y
+# mamba install wget -y
+# mamba install satpy -y
+# mamba install libgdal-hdf5 -y
+# mamba install gdal -y
 # mamba install jupyter -y
 # mamba install pywavelets -y
 # mamba install pytables -y
@@ -77,17 +66,19 @@ pip install f90nml
 # mamba install radian -y
 # mamba install jupyterlab -y
 # mamba install iris -y
-# pip install siphon
+
+# pip install SkillMetrics
+# pip install findent
+# pip install f90nml
+# pip install igra
 
 # pytest -v --pyargs xesmf
-# conda clean -a
 
 
 #--------------------------------
+# conda clean -a
 # conda env list
 # conda env remove -n rcm_gbr
 # conda config --show channels
 
-# conda create --prefix /g/data/v46/qg8515/conda_envs/test
-# conda activate /g/data/v46/qg8515/conda_envs/test
 # conda config --set auto_activate_base False   # remove (base) (base)
