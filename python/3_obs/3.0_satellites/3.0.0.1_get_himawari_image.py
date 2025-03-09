d8234c55dfcae9d9dbb2fd1be6c4c3984f04ffde
Qinggang Gao <gaoqg229@gmail.com>
2025-03-09 22:50:12 +1100

    9 Mar plot true color and night microphysics

--- python/3_obs/3.0_satellites/3.0.0.1_get_himawari_image.py
+++ python/3_obs/3.0_satellites/3.0.0.1_get_himawari_image.py
@@ -188,4 +188,90 @@ Thresholds: https://user.eumetsat.int/resources/case-studies/night-time-fog-over
 # endregion
 
 
+# region plot himawari data true color+night microphysics
+
+year, month, day, hour, minute = 2020, 7, 1, 8, 0
+dfolder = Path('/g/data/ra22/satellite-products/arc/obs/himawari-ahi/fldk/latest')
+# dfolder = Path('/g/data/ra22/satellite-products/nrt/obs/himawari-ahi/fldk/latest')
+
+opng = Path(f'figures/3_satellites/3.0_hamawari/3.0.0_image/3.0.0.0 himawari true_color and night_microphysics {year}{month:02d}{day:02d}{hour:02d}{minute:02d}.png')
+extent = [-5499500., 5499500., -5499500., 5499500.]
+transform = ccrs.Geostationary(central_longitude=140.7, satellite_height=35785863.0)
+bands=['B03', 'B02', 'B01', 'B07', 'B13', 'B15']
+
+plt_text = f'{year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d} UTC\nTrue Color and Night Microphysics RGB Himawari 8/9'
+
+channels = {}
+for iband in bands:
+    # iband='B07'
+    print(f'#-------------------------------- {iband}')
+    
+    ifile = sorted(Path(dfolder/f'{year}/{month:02d}/{day:02d}/{hour:02d}{minute:02d}').glob(f'*OBS_{iband}*'))
+    if not ifile:
+        print('Warning: No file found')
+        continue
+    
+    channels[iband] = Dataset(ifile[-1], 'r')
+    var_name = next(var for var in channels[iband].variables if var.startswith("channel_00"))
+    # print(f'#---------------- {var_name}')
+    channels[iband] = np.squeeze(channels[iband].variables[var_name][:])
+    
+    if iband in ['B03', 'B02', 'B01']:
+        channels[iband] = channels[iband].filled(0)
+    
+    if iband == 'B03':
+        channels[iband] = block_reduce(channels[iband], block_size=(2, 2), func=np.mean)
+
+channels['B15-B13'] = channels['B15']-channels['B13']
+channels['B13-B07'] = channels['B13']-channels['B07']
+
+channels['B15-B13'] = (channels['B15-B13'] + 4) / (2 + 4)
+channels['B13-B07'] = (channels['B13-B07'] + 0) / (10 + 0)
+channels['B13'] = (channels['B13'] - 243) / (293 - 243)
+
+for iband in ['B15-B13', 'B13-B07', 'B13']:
+    channels[iband] = channels[iband].filled(0)
+
+rgb1 = np.zeros(channels['B03'].shape + (3,), dtype=np.float32)
+for idx, iband in enumerate(['B03', 'B02', 'B01']):
+    rgb1[:, :, idx] = channels[iband]
+rgb1 = np.clip(rgb1, 0, 1, out=rgb1)
+rgb1 **= (1 / 2.2)
+
+rgb2 = np.zeros(channels['B15-B13'].shape + (3,), dtype=np.float32)
+for idx, iband in enumerate(['B15-B13', 'B13-B07', 'B13']):
+    rgb2[:, :, idx] = channels[iband]
+rgb2 = np.clip(rgb2, 0, 1, out=rgb2)
+rgb2 = np.kron(rgb2, np.ones((2, 2, 1)))
+
+rgb = np.where((rgb1.sum(axis=2) > 0.15)[..., None], rgb1, rgb2)
+
+
+fig, ax = plt.subplots(figsize=np.array([7, 7+1])/2.54, subplot_kw={'projection': transform})
+
+ax.imshow(rgb, extent=extent, transform=transform,
+          interpolation='none', origin='upper', resample=False)
+
+coastline = cfeature.NaturalEarthFeature(
+    'physical', 'coastline', '10m', edgecolor='yellow',
+    facecolor='none', lw=0.1)
+ax.add_feature(coastline, zorder=2, alpha=0.75)
+borders = cfeature.NaturalEarthFeature(
+    'cultural', 'admin_0_boundary_lines_land', '10m',
+    edgecolor='yellow', facecolor='none', lw=0.1)
+ax.add_feature(borders, zorder=2, alpha=0.75)
+gl = ax.gridlines(
+    crs=ccrs.PlateCarree(), lw=0.1, zorder=2, alpha=0.35,
+    color='yellow', linestyle='--',)
+gl.xlocator = mticker.FixedLocator(np.arange(0, 360 + 1e-4, 10))
+gl.ylocator = mticker.FixedLocator(np.arange(-90, 90 + 1e-4, 10))
+plt.text(0.5, -0.03, plt_text, transform=ax.transAxes, fontsize=8,
+         ha='center', va='top', rotation='horizontal', linespacing=1.5)
+
+fig.subplots_adjust(left=0.01, right=0.99, bottom=1/(7+1), top=0.99)
+fig.savefig(opng, transparent=True)
+plt.close()
+
+
+# endregion
 
